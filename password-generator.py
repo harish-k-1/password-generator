6be830dd6f5d1fee616eb9fd4451277f2bac1d72
Harish K <58143970+harish-k-1@users.noreply.github.com>
2022-04-14 20:05:26 +0530

    Added copy to clipboard feature

--- password-generator.py
+++ password-generator.py
@@ -1,31 +1,30 @@
 from random import *
-pass_len = int(input("Password Length: "))
-numbers = ['1','2','3','4','5','6','7','8','9','0']
-lchar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-uchar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-symbols = ['!','@','#','$','%','^','&','*']
-combined = numbers + lchar + uchar + symbols
+from string import *
+import pyperclip
+import time
 
-rand_num=choice(numbers)
-rand_lchar=choice(lchar)
-rand_uchar=choice(uchar)
-rand_sym=choice(symbols)
-temp_pass = rand_num + rand_lchar + rand_uchar + rand_sym
+pass_len = int(input("Password Length: "))
+numbers = digits
+lchar = ascii_lowercase
+uchar = ascii_uppercase
+symbols = "!@#$%^&*"
+combined = numbers + lchar + uchar + symbols
+temp_pass = choice(numbers)+choice(lchar)+choice(uchar)+choice(symbols)
 
 for i in range(pass_len-4):
-    temp_pass = temp_pass + choice(combined)  #temp_pass is a string
+    temp_pass = temp_pass + choice(combined)
 
-arr=[]
-for i in temp_pass:
-    arr.append(i)    #convert to list and shuffle
+arr=list(temp_pass)  #convert to list and shuffle
 shuffle(arr)
-
 passw=''.join(arr)  #again converting back to string
 print(passw)
 
 inp=input("Do you want to save this password?(Y/N)\t")
-if inp == "Y" or "y":
+if inp == "Y" or inp == "y":
     f = open("passfile.txt","a")
     note = input("Leave a note:\t")
-    f.write(f"\n{note}: {passw}")
+    f.write(f"{note}: {passw}\n")
+    pyperclip.copy(passw)
+    print("Password copied to clipboard")
     f.close()
+    time.sleep(10)
