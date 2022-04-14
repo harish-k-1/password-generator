eede0349290df78e87191e3e4d151df6573a2600
Harish K <58143970+harish-k-1@users.noreply.github.com>
2022-04-14 20:27:20 +0530

    Added option to ask another password

--- password-generator.py
+++ password-generator.py
@@ -3,28 +3,33 @@ from string import *
 import pyperclip
 import time
 
-pass_len = int(input("Password Length: "))
-numbers = digits
-lchar = ascii_lowercase
-uchar = ascii_uppercase
-symbols = "!@#$%^&*"
-combined = numbers + lchar + uchar + symbols
-temp_pass = choice(numbers)+choice(lchar)+choice(uchar)+choice(symbols)
+def generate_password(pass_len):
+    symbols = "!@#$%^&*"
+    combined = digits + ascii_lowercase + ascii_uppercase + symbols
+    temp_pass=choice(digits)+choice(ascii_lowercase)+choice(ascii_uppercase)+choice(symbols)
 
-for i in range(pass_len-4):
-    temp_pass = temp_pass + choice(combined)
+    for i in range(pass_len-4):
+        temp_pass += choice(combined)
 
-arr=list(temp_pass)  #convert to list and shuffle
-shuffle(arr)
-passw=''.join(arr)  #again converting back to string
-print(passw)
+    arr=list(temp_pass)  #convert to list in order to shuffle
+    shuffle(arr)
+    passw=''.join(arr)  #again converting back to string
+    print(passw)
 
-inp=input("Do you want to save this password?(Y/N)\t")
-if inp == "Y" or inp == "y":
-    f = open("passfile.txt","a")
-    note = input("Leave a note:\t")
-    f.write(f"{note}: {passw}\n")
-    pyperclip.copy(passw)
-    print("Password copied to clipboard")
-    f.close()
-    time.sleep(10)
+    inp=input("Do you want to save this password?(Y/N)\t")
+    if inp == "Y" or inp == "y":
+        f = open("passfile.txt","a")
+        note = input("Leave a note:\t")
+        f.write(f"{note}: {passw}\n")
+        pyperclip.copy(passw)
+        print("Password copied to clipboard")
+        f.close()
+        time.sleep(10)
+    else:
+        inp2=input("Do you want another password?(Y/N)\t")
+        if inp2=="Y" or inp2=="y":
+            generate_password(pass_len)
+
+if __name__ == "__main__":
+    pass_len = int(input("Password Length: "))
+    generate_password(pass_len)
